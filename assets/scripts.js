aff70421e11117508c5c7efc4b06992abc2a4cec
garima675 <garimajain.0725@gmail.com>
2024-07-25 16:05:31 +0200

    added hazy loading to images,compressed some images
    added hazy loading to images,compressed some images

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,45 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Create an intersection observer
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
     });
+
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Fallback for older browsers
+    lazyImages.forEach(function (lazyImage) {
+      lazyImage.src = lazyImage.dataset.src;
+      lazyImage.classList.remove("lazy");
+    });
+  }
 });
